cb9444183e79f1572d25f2b56a64d1a4037c23fe
Dokyun Claire Lim <im.koremp@gmail.com>
2024-03-23 18:42:08 +0900

    test: add text input test

---
+++ src/components/__tests__/TextInput.test.tsx
@@ -0,0 +1,14 @@
+import { getByLabelText, render } from '@testing-library/react';
+
+import TextInput from '../TextInput';
+
+describe("TextInput", () => {
+  it("renders label.", () => {
+    const label = "label example";
+    const { container } = render(
+      <TextInput label={label} />
+    );
+
+    expect(container).toHaveTextContent(label);
+  });
+});
