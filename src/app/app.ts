6028ac3e06b64b84cf6e2cd3dc35bfeb1250e06f
Dokyun Claire Lim <im.koremp@gmail.com>
2024-03-24 16:02:41 +0900

    feat: add account reducer to store

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import accountReducer from './reducers/account';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    account: accountReducer,
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
